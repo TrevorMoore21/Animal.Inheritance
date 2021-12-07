90d8b30eafb05b66e6e90222de24f5a6feb05932
trevor.moore <trevor.moore@MU-C1-03.mullen.pvt.k12.co.us>
2021-12-07 09:00:58 -0700

    stop and come back later

--- Animal.Inheritance/src/Animals.java
+++ Animal.Inheritance/src/Animals.java
@@ -1,16 +1,22 @@
 
-public class Animals
+public abstract class Animals
 	{
+		private String type = "animal";
+		private String food = "sustinance";
 		
-		public static void Animal()
-		{
-			
-			
-			
-			
-			
-		}
+		public abstract void bearsYoung();
+			{
+				System.out.println();
+			}
 		
-		
+		public abstract void eats();
+			{
+				System.out.println();
+			}
+			
+		public abstract void makesNoise();
+			{
+				System.out.println();
+			}
 		
 	}
